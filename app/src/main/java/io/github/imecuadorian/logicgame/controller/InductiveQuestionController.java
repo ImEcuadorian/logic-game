de4fc6fdd2db813cc1d19015d23faae6e0eeff22
Istinger <jossuepatric@hotmail.com>
2024-07-10 18:59:34 -0500

    Inductive question finished

--- app/src/main/java/io/github/imecuadorian/logicgame/controller/InductiveQuestionController.java
+++ app/src/main/java/io/github/imecuadorian/logicgame/controller/InductiveQuestionController.java
@@ -1,8 +1,33 @@
 package io.github.imecuadorian.logicgame.controller;
 
+import io.github.imecuadorian.logicgame.model.DeductiveQuestion;
 import io.github.imecuadorian.logicgame.model.InductiveQuestion;
 import io.github.imecuadorian.logicgame.services.InductiveQuestionService;
-
+import lombok.AllArgsConstructor;
+import java.util.List;
+@AllArgsConstructor
 public class InductiveQuestionController {
+
+	private final InductiveQuestionService inductiveQuestionService;
+
+	public List<InductiveQuestion> getAll() {
+		return inductiveQuestionService.getAll();
+	}
+
+	public InductiveQuestion get(Integer id) {
+		return inductiveQuestionService.get(id);
+	}
+
+	public InductiveQuestion save(InductiveQuestion entity) {
+		return inductiveQuestionService.save(entity);
+	}
+
+	public void delete(Integer id) {
+		inductiveQuestionService.delete(id);
+	}
+
+	public int getSize() {
+		return inductiveQuestionService.getAll().size();
+	}
 
 }
